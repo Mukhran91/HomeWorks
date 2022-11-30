68e2cbeff5e4dec91afa7716441fba1bbea57820
Mukhrani <mukhran91@gmail.com>
2022-11-30 16:37:02 +0400

    HomeWork2

---
+++ HomeWork_2/task2/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter X : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int min, max;
+            
+            if (x > y)
+            {
+                max = x;
+                min = y;
+            }
+            else
+            {
+                max = y;
+                min = x; 
+            }
+            
+            Console.WriteLine(max + min);
+            Console.WriteLine(max - min);
+            Console.WriteLine(max * min);
+
+            if (min == 0)
+            {
+                Console.WriteLine("Not Allowed To Divide By Zero");
+            }
+            else
+            {
+                Console.WriteLine(max / min);
+            }
+        }
+    }
+}
---
+++ HomeWork_2/task3/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int TMP = x;
+            x = y;
+            y = TMP;
+            
+            Console.WriteLine($"x = {x} y = {y}");
+        }
+    }
+}
